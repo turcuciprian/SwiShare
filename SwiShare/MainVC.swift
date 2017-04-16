d9eaa48bd4099f2e334eed88cde969edc3bd3907
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-16 14:15:25 +0300

    imported avfoundation declared a few global variables added current capture device on viewDidLoad

--- SwiShare/MainVC.swift
+++ SwiShare/MainVC.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession = AVCaptureSession()
+            
+            captureSession?.addInput(input)
+        }
     }
 
     override func didReceiveMemoryWarning() {
