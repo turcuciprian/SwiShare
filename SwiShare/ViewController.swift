647308083739c48b77355bdf133c2473b5fa5e65
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-16 13:58:01 +0300

    Initial Commit

---
+++ SwiShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiShare
+//
+//  Created by Turcu Ciprian on 16/04/2017.
+//  Copyright © 2017 ToolAxy One S.R.L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
